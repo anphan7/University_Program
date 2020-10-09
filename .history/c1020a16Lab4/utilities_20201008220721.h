79a8d13be0f0c9c15fa0e3a216b71e15a858b5b4
xmrtonyx125 <69742686+xmrtonyx125@users.noreply.github.com>
2020-10-08 22:09:07 -0400

    fix

---
+++ .history/c1020a16Lab4/utilities_20201008220721.h
@@ -0,0 +1,14 @@
+/*
+ * Name : Peter Phan
+ * Due Date: 4/23/2019
+ * File name: utilities.h
+ * Desription: this file perform some small task during the program 
+ */ 
+#ifndef ULTILIES_H
+#define ULTILIES_H
+#define TRUE 1
+#define FALSE 0
+void to_Lower (char[] ){};
+void to_Upper (char[] ){};
+void for_space(){};
+#endif
--- c1020a16Lab4/utilities.h
+++ c1020a16Lab4/utilities.h
@@ -8,9 +8,7 @@
 #define ULTILIES_H
 #define TRUE 1
 #define FALSE 0
-void to_Lower (char[] );
-void to_Upper (char[] );
-void clearBuffer (FILE*);
-int checkDigit (char[]);
-void for_space();
+void to_Lower (char[] ){};
+void to_Upper (char[] ){};
+void for_space(){};
 #endif
